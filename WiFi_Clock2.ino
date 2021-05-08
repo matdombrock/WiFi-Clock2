5bcfeaa2aa2fd5e526106d74e3849fb699a135c9
Mathieu Dombrock <matdombrock@gmail.com>
2021-05-08 07:00:00 -0700

    Remove example routes

--- WiFi_Clock2.ino
+++ WiFi_Clock2.ino
@@ -52,20 +52,6 @@ void setup() {
     * void setupBP(const char *apName="ESP SETUP", const char *ssid=NULL, const char *pkey=NULL)
     */
     setupBP("WiFiClock", "HERSHEL", "meowmeow");// Required
-    // EXAMPLE ROUTES
-    // Check system uptime
-    server.on("/uptime", HTTP_GET, r_uptime);
-    // Test reading content from PROGMEM
-    server.on("/test", HTTP_GET, r_test);
-    // Scan local WIFI networks
-    server.on("/scan", HTTP_GET, r_scan);
-    // Get system status info
-    server.on("/status", HTTP_GET, r_status);
-    // Send a GET request to <IP>/get?message=<message>
-    server.on("/get", HTTP_GET, r_get);
-    // Send a POST request to <IP>/post with a form field message set to <message>
-    server.on("/post", HTTP_POST, r_post);
-    //your other setup stuff...
 
     st.lenX = matrix.lenX()-1;
     st.lenY =  matrix.lenY()-1;
